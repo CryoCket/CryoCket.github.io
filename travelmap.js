99febbd4ef5693522b079ae27bbe8b13596455f6
Chase Mobley <chasemobley50@gmail.com>
2026-07-09 13:43:00 -0400

    Initialize travel map with Hokkaido coordinates

---
+++ travelmap.js
@@ -0,0 +1,8 @@
+//43°28' N latitude and 142°31' E longitude
+var coordinates = [43°28', 142°31];
+var map = L.map('map').setView(coordinates, 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker(coordinates).bindPopup("<p>Hello, from Hokkaido Japan!</p>").addTo(map);
