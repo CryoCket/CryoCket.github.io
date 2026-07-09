7809d3a49713cd8266ff8a14ad1370ec3f8f48f9
Chase Mobley <chasemobley50@gmail.com>
2026-07-09 14:19:20 -0400

    Add markers for multiple locations on the map

--- travelmap.js
+++ travelmap.js
@@ -9,4 +9,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from Hokkaido Japan!</p>").addTo(map);
-var marker1 = L.circle(place1,{color; "black", fillColor: "#fc0394", radius: 80}).addTo(map);
+var marker1 = L.circle(place1,{color; "black", fillColor: "#fc0394", radius: 90}).bindPopup("<p>Hello from Asahiyama Zoo!</p>").addTo(map);
+var marker2 = L.circle(place2,{color; "black", fillColor: "#fc0394", radius: 90}).bindPopup("<p>Hello from Odori Park!</p>").addTo(map);
+var marker3 = L.circle(place3,{color; "black", fillColor: "#fc0394", radius: 90}).bindPopup("<p>Hello from Tanukikoji Shopping Street</p>").addTo(map);
